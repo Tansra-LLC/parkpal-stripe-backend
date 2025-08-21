c6739da4cd3fd18b7524f167653b8b03601c7e76
Tansra-LLC <tansra24@gmail.com>
2025-08-21 16:48:34 -0400

    Update migrate.js

--- migrate.js
+++ migrate.js
@@ -2,10 +2,7 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
 async function main() {
-  const db = await open({
-    filename: "./data.db",
-    driver: sqlite3.Database
-  });
+  const db = await open({ filename: "./data.db", driver: sqlite3.Database });
 
   await db.exec(`
     CREATE TABLE IF NOT EXISTS users (
